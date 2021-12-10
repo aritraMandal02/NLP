4dd7ebfd8b5e9a6d864dc391eb6f7a2ec2190080
Aritra Mandal <aritra.mandal2002@gmail.com>
2021-12-10 10:46:26 +0530

    Create tut1.py

---
+++ tut1.py
@@ -0,0 +1,30 @@
+from typing import Sequence
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentences = [
+    'i love my dog',
+    'I, love my cat',
+    'You love my dog!',
+    'Do you think my dog is amazing?'
+]
+
+test_data = [
+    'I really love my dog.',
+    'My dog loves me.'
+]
+
+tokenizer = Tokenizer(num_words=100, oov_token="<OOV>")
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(sentences)
+padded = pad_sequences(sequences, padding='post', truncating='post', maxlen=5)
+
+test_seq = tokenizer.texts_to_sequences(test_data)
+print(test_seq)
+
+print(word_index)
+print(sequences)
+print(padded)
