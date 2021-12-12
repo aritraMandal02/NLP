3ce720ebcc0d836d58c3b65cc82ca0da2b5c2630
Aritra Mandal <aritra.mandal2002@gmail.com>
2021-12-12 16:20:59 +0530

    setting suitable version

---
+++ tut2.py
@@ -0,0 +1,69 @@
+# classifying text
+#!pip install tensorflow==2.4
+import json
+import tensorflow as tf
+from tensorflow.keras.preprocessing import sequence
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+vocab_size = 10000
+embedding_dim = 16
+max_length = 100
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
+training_size = 20000
+
+with open("sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+# Need this block to get it to work with TensorFlow 2.x
+import numpy as np
+training_padded = np.array(training_padded)
+training_labels = np.array(training_labels)
+testing_padded = np.array(testing_padded)
+testing_labels = np.array(testing_labels)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length), # direction of each word will be learnt epoch by epoch
+    tf.keras.layers.GlobalAveragePooling1D(), 
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# model.summary()
+
+num_epochs = 30
+history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
+
+sentence = ["granny starting to fear spiders in the garden might be real", "game of thrones season finale showing this sunday night", "Be sarcastic don't kill.",
+ "Silence is golden. Duct tape is silver.", "It's okay if you don't like me. Not everyone has good taste."]
+sequences = tokenizer.texts_to_sequences(sentence)
+padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+print(model.predict(padded)) # gives the probability
